f3eefed02736488e795d6f0e30f546d617671854
Turag Ikbal <garutturag@gmail.com>
2026-02-08 16:12:02 -0500

    update main.rs to include argument handling and directory parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use walkdir::{DirEntry, WalkDir};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    path: String,
+}
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry
+        .file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    WalkDir::new(args.path)
+        .into_iter()
+        .filter_entry(|e| !is_hidden(e))
+        .filter_map(|e| e.ok())
+        .for_each(|entry| {
+            if entry.file_type().is_file() {
+                println!("{}", entry.path().display());
+            }
+        });
 }
